890b6465e9b83bf5712ee758b8b707cd646852eb
Luke Davis <davislccg@gmail.com>
2020-12-16 23:39:16 +0200

    Fixed irregular use of tabs and spaces in main.cpp

--- main.cpp
+++ main.cpp
@@ -19,39 +19,39 @@ void sigIntHandler(int s){
 
 //an implementation of the Java Random() RNG
 class JavaRandom {
-	public:
-	typedef int result_type;
-	uint64_t seed=1;
-	JavaRandom(uint64_t s){
-		seed=s;
-	}
-	static constexpr int min(){
-		return 1;
-	}
-	static constexpr int max(){
-		return 2147483647;
-	}
-	inline result_type next(int bits){
-		seed=((seed)*0x5DEECE66DL+0xBL)&((1L << 48) - 1);
-		return seed >> (48 - bits);
-	}
-	inline result_type nextInt(){
-		return next(32);
-	}
-	
-	result_type operator()() {
+    public:
+    typedef int result_type;
+    uint64_t seed=1;
+    JavaRandom(uint64_t s){
+        seed=s;
+    }
+    static constexpr int min(){
+        return 1;
+    }
+    static constexpr int max(){
+        return 2147483647;
+    }
+    inline result_type next(int bits){
+        seed=((seed)*0x5DEECE66DL+0xBL)&((1L << 48) - 1);
+        return seed >> (48 - bits);
+    }
+    inline result_type nextInt(){
+        return next(32);
+    }
+    
+    result_type operator()() {
         return nextInt();
-	}
+    }
 };
 
 int main(int argc, char * argv[])
 {
     srand(time(0));
-	bool timer=false;
-	
-	if(argc>=2 && string(argv[1]).compare("-t")==0){
-		timer=true;
-	}
+    bool timer=false;
+    
+    if(argc>=2 && string(argv[1]).compare("-t")==0){
+        timer=true;
+    }
     signal(SIGINT, sigIntHandler);
 
     std::ofstream file;
@@ -102,20 +102,20 @@ int main(int argc, char * argv[])
     }
     
     JavaRandom j(123);
-  	std::binomial_distribution<int> distribution(262,20.0/423);
-  	std::binomial_distribution<int> distribution2(305,0.5);
-	auto start = high_resolution_clock::now(); 
+    std::binomial_distribution<int> distribution(262,20.0/423);
+    std::binomial_distribution<int> distribution2(305,0.5);
+    auto start = high_resolution_clock::now(); 
     while(running) {
         iterations++;
-		if(timer==true && iterations==TIMED_ITERATION){
-			auto stop = high_resolution_clock::now(); 
-			auto duration = duration_cast<microseconds>(stop - start); 
-			int m=duration.count();
-			std::cout.clear();
-			system("clear");
-			std::cout << "Took " << m/1000000.0<< " to perform "<< TIMED_ITERATION<<" iterations"<< std::endl; 
-			return 0;
-		}
+        if(timer==true && iterations==TIMED_ITERATION){
+            auto stop = high_resolution_clock::now(); 
+            auto duration = duration_cast<microseconds>(stop - start); 
+            int m=duration.count();
+            std::cout.clear();
+            system("clear");
+            std::cout << "Took " << m/1000000.0<< " to perform "<< TIMED_ITERATION<<" iterations"<< std::endl; 
+            return 0;
+        }
         int64_t numPearls = distribution(j);
         
         pearlInstances[numPearls]++;
@@ -129,24 +129,24 @@ int main(int argc, char * argv[])
         maxRods = std::max(maxRods, numRods);
         if(iterations%10000000==0){
             // Iterations Message
-        	std::cout << ansi::cursor_pos(4, iterationsMsg.length() + 1) << ansi::erase_in_line() << iterations << std::endl;
-			// Pearl Message
-        	std::cout << ansi::cursor_pos(14, pearlMsg.length() + 1) << ansi::erase_in_line() << numPearls << std::endl;
-			 // Rods Message
-        	std::cout << ansi::cursor_pos(15, rodsMsg.length() + 1) << ansi::erase_in_line() << numRods << std::endl;
+            std::cout << ansi::cursor_pos(4, iterationsMsg.length() + 1) << ansi::erase_in_line() << iterations << std::endl;
+            // Pearl Message
+            std::cout << ansi::cursor_pos(14, pearlMsg.length() + 1) << ansi::erase_in_line() << numPearls << std::endl;
+            // Rods Message
+            std::cout << ansi::cursor_pos(15, rodsMsg.length() + 1) << ansi::erase_in_line() << numRods << std::endl;
 
-       		// Pearl Avg Message
-        	pearlAvg = pearlSum * 1.0 / iterations;
-        	std::cout << ansi::cursor_pos(5, pearlAvgMsg.length() + 1) << ansi::erase_in_line() << pearlAvg << std::endl;
-        	// Rods Avg Message
-        	rodsAvg = rodsSum * 1.0 / iterations;
-        	std::cout << ansi::cursor_pos(6, rodsAvgMsg.length() + 1) << ansi::erase_in_line() << rodsAvg << std::endl;
-        	// Pearl Max Message
-        	std::cout << ansi::cursor_pos(7, pearlMaxMsg.length() + 1) << ansi::erase_in_line() << maxPearls << std::endl;
-        	// Rods Max Message
-        	std::cout << ansi::cursor_pos(8, rodsMaxMsg.length() + 1) << ansi::erase_in_line() << maxRods << std::endl;
-		}
-       
+            // Pearl Avg Message
+            pearlAvg = pearlSum * 1.0 / iterations;
+            std::cout << ansi::cursor_pos(5, pearlAvgMsg.length() + 1) << ansi::erase_in_line() << pearlAvg << std::endl;
+            // Rods Avg Message
+            rodsAvg = rodsSum * 1.0 / iterations;
+            std::cout << ansi::cursor_pos(6, rodsAvgMsg.length() + 1) << ansi::erase_in_line() << rodsAvg << std::endl;
+            // Pearl Max Message
+            std::cout << ansi::cursor_pos(7, pearlMaxMsg.length() + 1) << ansi::erase_in_line() << maxPearls << std::endl;
+            // Rods Max Message
+            std::cout << ansi::cursor_pos(8, rodsMaxMsg.length() + 1) << ansi::erase_in_line() << maxRods << std::endl;
+        }
+    
     }
 
     //      Iterations                         Pearl Avg          Rods Avg          Pearls Max          Rods Max
